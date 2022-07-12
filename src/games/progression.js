92108832a770edc64ccde6c4ceb81a1336255f71
Islam Tsokuev <cokuevn@gmail.com>
2022-07-12 17:21:19 +0300

    add game

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,24 +2,24 @@ import gameRun from '../index.js';
 import getRandomInt from '../getRandomNum.js';
 
 const descriptionGame = 'What number is missing in the progression?';
-const progressionLength = 10;
+const lengthOfProgression = 10;
 
-const getProgression = (start, diff, length) => {
-  let progression = [];
-  for (let i = 0; i < length; i += 1) {
-    progression = [...progression, start + diff * i];
-  }
-  return progression;
+const getProgression = (start, diff, length, ind) => {
+  const filledArray = Array.apply(0, Array(length)).map(() => (start));
+  const arrayWithProgression = filledArray.map((el, index) => {
+    const result = el + diff * index;
+    return index === ind ? '..' : result;
+  }, 1);
+  return arrayWithProgression.join(' ');
 };
 
 const getTaskData = () => {
   const start = getRandomInt();
-  const diff = getRandomInt(1, 9);
-  const secretNum = getRandomInt(0, progressionLength);
-  const progression = getProgression(start, diff, progressionLength);
-  const correctAnswer = `${progression.splice(secretNum, 1, '..')}`;
-  const question = `${progression.join(' ')}`;
-  return [question, correctAnswer];
+  const diff = getRandomInt(1, 10);
+  const indexOfHiddenValue = getRandomInt(0, lengthOfProgression - 1);
+  const question = getProgression(start, diff, lengthOfProgression, indexOfHiddenValue);
+  const answer = `${start + diff * indexOfHiddenValue}`;
+  return [question, answer];
 };
 
 const startProgressionGame = () => gameRun(descriptionGame, getTaskData);
